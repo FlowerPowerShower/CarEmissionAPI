43052b70807949cb81a5dbbd7633f1ef2ef93f5f
FlowerPowerShower <56584705+FlowerPowerShower@users.noreply.github.com>
2022-01-17 22:21:09 +0100

    Just a test commit to push new branch to heroku

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 8002;
+const port = 8002;
 const express = require("express")
 const axios = require("axios")
 const cheerio = require("cheerio")
@@ -6,17 +6,18 @@ const fs = require('fs');
 
 const app = express()
 
+
 var carslist = []
 
-app.get("/"), (req,res) => {
-    res.json("This API is in beta, use /GetCars to retrive a list of cars, more functionality to come")
-}
+app.get("/", (req,res) => {
+    res.json("Welcome to this API(beta) more functionality to come, get cars with /GetCars")
+})
 
     
 app.get("/GetCars", (req,res) => {
     
     
-    fs.readFileSync('cars.txt', function(err, data) {
+    fs.readFile('cars.txt', function(err, data) {
         if(err) throw err;
         var data = (data.toString().replace(/['"]+/g, ''));
         var array = data.toString().split(",");
@@ -26,39 +27,33 @@ app.get("/GetCars", (req,res) => {
         model = array[i],
         engine =array[i+=1],
         offical_Euro_Class= array[i+=1],
-        WTW_C02= array[i+=1],
-        TTW_CO2 = array[i+=1],
+        WTW_C02a= array[i+=1],
+        WTW_CO2 = parseInt(WTW_C02a),
+        TTW_CO2a = array[i+=1],
+        TTW_CO2 = parseInt(TTW_CO2a)
+        if(TTW_CO2 <= 0){
+            TTW_CO2 = "NA"
+        }
         TestProg = array[i+=1]
+  
+
         carslist.push({
             model,
             engine,
             offical_Euro_Class,
-            WTW_C02,
+            WTW_CO2,
             TTW_CO2,
             TestProg
-        })   
-        
+        })  
+  
     }
-    carslist = carslist.map(function(obj) {
-        return {
-            model: obj.model, 
-            engine: obj.engine,
-            offical_Euro_Class : obj.offical_Euro_Class,
-            WTW_C02 : parseInt(obj.WTW_C02),
-            TTW_c02 : parseInt(obj.TTW_CO2),
-            TestProg : obj.TestProg
-        };
+        
     });
-    res.json(carslist)
+       res.json(carslist)
  
 });
-})
 
-app.get("/GetCars/:model", (req,res) => {
-    
-    res.json(req.body)
- 
-}) 
+
 
 
 
